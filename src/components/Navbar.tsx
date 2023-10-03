455d254c6e02eae2d6b2e406c8734c38c82b1d02
Prateek Chaurasia <prateek18801@gmail.com>
2023-10-03 22:03:08 +0530

    Fix background stretch

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 const Navbar = () => {
     return (
-        <nav className="fixed w-full max-w-5xl flex justify-between items-center py-4 text-sm">
-            <div className="text-2xl font-semibold">PC</div>
-            <ul className="flex">
-                <li className="ml-9">Home</li>
-                <li className="ml-9">About</li>
-                <li className="ml-9">Services</li>
-                <li className="ml-9">Portfolio</li>
-                <li className="ml-9">Contact</li>
-            </ul>
+        <nav className="fixed flex justify-center w-full">
+            <div className="flex items-center justify-between w-full max-w-5xl py-4 text-sm ">
+                <div className="text-2xl font-semibold">PC</div>
+                <ul className="flex">
+                    <li className="ml-9">Home</li>
+                    <li className="ml-9">About</li>
+                    <li className="ml-9">Services</li>
+                    <li className="ml-9">Portfolio</li>
+                    <li className="ml-9">Contact</li>
+                </ul>
+            </div>
         </nav>
     );
 }
