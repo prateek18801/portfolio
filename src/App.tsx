2e09d52fa88214563ead5c42470352535a2319d3
Prateek Chaurasia <prateek18801@gmail.com>
2023-10-03 22:06:46 +0530

    Import components in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Services from "./pages/Services";
+
 const App = () => {
     return (
-        <>Hello World</>
+        <>
+            <Navbar />
+            <div className="w-full max-w-5xl">
+                <Home />
+                <About />
+                <Services />
+                <Portfolio />
+                <Contact />
+            </div>
+            <Footer />
+        </>
     );
-}
+};
 
 export default App;
