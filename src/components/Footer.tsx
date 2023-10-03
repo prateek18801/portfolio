777e49beb372dac7f7da8ae0619c289ebc6e83eb
Prateek Chaurasia <prateek18801@gmail.com>
2023-10-03 22:04:22 +0530

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer className="flex justify-center w-full text-white bg-black">
+            <div className="w-full max-w-5xl">
+                Prateek Chaurasia
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
