d5bed7893ae17d414538a5c06638dd290447d374
Prateek Chaurasia <prateek18801@gmail.com>
2023-10-07 22:40:25 +0530

    Complete contact section

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,40 +1,36 @@
 import { FaLocationDot } from "react-icons/fa6";
 import { PiPaperPlaneTiltBold } from "react-icons/pi";
 import { BsFillEnvelopeAtFill } from "react-icons/bs";
+import { ReactNode } from "react";
+
+type ContactChipProps = {
+    icon: ReactNode,
+    field: string,
+    value: string,
+    href: string
+}
 
 const Contact = () => {
     return (
         <section>
             <div className="section-heading">Get in touch</div>
             <div className="flex gap-16 mx-24">
-                <div className="flex flex-col justify-center w-1/2 mb-20">
+                <div className="flex flex-col justify-center w-1/2 mb-10">
                     <div className="mb-10 text-xl font-medium">Tell me about your project. Let's create something together.</div>
-                    <div className="flex mb-6">
-                        <div className="flex items-center justify-center p-4 mr-3 text-2xl text-white rounded-full bg-zinc-800"><BsFillEnvelopeAtFill /></div>
-                        <div>
-                            <div className="text-xl font-medium">Email</div>
-                            <div>prateek18801@gmail.com</div>
-                        </div>
-                    </div>
-                    <div className="flex">
-                        <div className="flex items-center justify-center p-4 mr-3 text-2xl text-white rounded-full bg-zinc-800"><FaLocationDot /></div>
-                        <div>
-                            <div className="text-xl font-medium">Location</div>
-                            <div>Delhi, India</div>
-                        </div>
-                    </div>
+                    <ContactChip icon={<BsFillEnvelopeAtFill />} field="Email" value="prateek18801@gmail.com" href="mailto:prateek18801@gmail.com" />
+                    <ContactChip icon={<FaLocationDot />} field="Location" value="Delhi, India" href="https://maps.app.goo.gl/tAa3Hyt2mcm7aohk6" />
                 </div>
                 <form className="w-1/2">
                     {/* <div className="mb-4 text-lg font-medium text-center text-zinc-800">Send me a message</div> */}
-                    <div className="relative h-16 mb-4 border-2 border-neutral-400 rounded-xl">
+                    <div className="relative h-16 mb-4 border border-neutral-400 rounded-xl">
                         <input type="text" className="w-full h-full px-4 bg-transparent outline-none peer" placeholder=" " required />
                         <label htmlFor="name" className="absolute px-2 left-2 -top-[13px] pointer-events-none transition-all duration-300 scale-75 origin-left bg-white text-neutral-500 peer-placeholder-shown:top-[18.5px] peer-placeholder-shown:scale-100 peer-focus:-top-[13px] peer-focus:scale-75">Name</label>
                     </div>
-                    <div className="relative h-16 mb-4 border-2 border-neutral-400 rounded-xl">
+                    <div className="relative h-16 mb-4 border border-neutral-400 rounded-xl">
                         <input type="email" className="w-full h-full px-4 bg-transparent outline-none peer" placeholder=" " required />
                         <label htmlFor="name" className="absolute px-2 left-2 -top-[13px] pointer-events-none transition-all duration-300 scale-75 origin-left bg-white text-neutral-500 peer-placeholder-shown:top-[18.5px] peer-placeholder-shown:scale-100 peer-focus:-top-[13px] peer-focus:scale-75">Email</label>
                     </div>
-                    <div className="relative mb-4 border-2 h-36 border-neutral-400 rounded-xl">
+                    <div className="relative mb-4 border h-36 border-neutral-400 rounded-xl">
                         <textarea className="w-full h-full px-4 py-4 bg-transparent outline-none resize-none peer" placeholder=" " required />
                         <label htmlFor="name" className="absolute px-2 left-2 -top-[13px] pointer-events-none transition-all duration-300 scale-75 origin-left bg-white text-neutral-500 peer-placeholder-shown:top-[18.5px] peer-placeholder-shown:scale-100 peer-focus:-top-[13px] peer-focus:scale-75">Message</label>
                     </div>
@@ -45,6 +41,20 @@ const Contact = () => {
                 </form>
             </div>
         </section>
+    );
+}
+
+const ContactChip = ({ icon, field, value, href }: ContactChipProps) => {
+    return (
+        <div className="flex mb-6">
+            <a href={href} target="_blank">
+                <div className="flex items-center justify-center p-4 mr-3 text-2xl text-white rounded-full cursor-pointer bg-zinc-800 hover:bg-black">{icon}</div>
+            </a>
+            <div>
+                <div className="text-xl font-medium">{field}</div>
+                <div>{value}</div>
+            </div>
+        </div>
     );
 }
 
