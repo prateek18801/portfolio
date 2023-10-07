5553fe3b0a96f7e723a3648cbb7759cc0ee41f9c
Prateek Chaurasia <prateek18801@gmail.com>
2023-10-08 01:26:54 +0530

    Complete footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,26 @@
-const Footer = () => {
+import { BsLinkedin, BsInstagram, BsGithub, BsWhatsapp } from "react-icons/bs";
+
+const Footer = ({ scrollToSection }: { scrollToSection: (s: number) => void }) => {
     return (
-        <footer className="flex justify-center w-full text-white bg-black">
-            <div className="w-full max-w-5xl">
-                Prateek Chaurasia
+        <footer className="flex flex-col items-center w-full py-8 mt-16 text-white bg-zinc-900">
+            <div className="flex justify-between w-full max-w-5xl">
+                <div className="flex flex-col mb-8 text-2xl font-semibold">
+                    Prateek Chaurasia
+                    <div className="text-sm font-normal">Web Developer</div>
+                </div>
+                <ul className="flex gap-6">
+                    <li className="cursor-pointer" onClick={() => { scrollToSection(2) }}>Services</li>
+                    <li className="cursor-pointer" onClick={() => { scrollToSection(3) }}>Portfolio</li>
+                    <li className="cursor-pointer" onClick={() => { scrollToSection(4) }}>Contact</li>
+                </ul>
             </div>
+            <div className="flex gap-4 mb-6 text-lg">
+                <a href="https://www.linkedin.com/in/prateekchaurasia/"><BsLinkedin /></a>
+                <a href="https://github.com/prateek18801/"><BsGithub /></a>
+                <a href="https://wa.me/+918726127335/"><BsWhatsapp /></a>
+                <a href="https://www.instagram.com/pc.incognito/"><BsInstagram /></a>
+            </div>
+            <div className="text-xs">&copy; Prateek Chaurasia. All rights reserved.</div>
         </footer>
     );
 }
