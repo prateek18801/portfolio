51213f181cf6287aa390871539477a42ee14b234
Prateek Chaurasia <prateek18801@gmail.com>
2023-10-26 09:16:13 +0530

    Remove outline

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -7,7 +7,7 @@ type ScrollToTopProps = {
 
 const ScrollToTop = ({ activeSection, scrollToSection }: ScrollToTopProps) => {
     return (
-        <button className={`fixed p-1 text-3xl text-white transition-all bg-black rounded-full opacity-80 hover:opacity-100 right-12 bottom-12 ${activeSection > 0 ? "scale-100" : "scale-0"}`} onClick={() => { scrollToSection(0) }}>
+        <button className={`fixed right-12 bottom-12 p-1 text-3xl text-white bg-black rounded-full opacity-80 transition-all outline-none hover:opacity-100 ${activeSection > 0 ? "scale-100" : "scale-0"}`} onClick={() => { scrollToSection(0) }}>
             <MdKeyboardArrowUp />
         </button>
     );
