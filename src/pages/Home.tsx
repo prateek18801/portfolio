b7adade37aa977b51b347ef3e7b7292bc08a5b47
Prateek Chaurasia <prateek18801@gmail.com>
2023-11-03 21:42:49 +0530

    Add image fix mobile view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,25 @@
 import { PiPaperPlaneTiltBold } from "react-icons/pi";
 import { BsLinkedin, BsInstagram, BsGithub, BsWhatsapp, BsMouse } from "react-icons/bs";
+import HomeImg from "../assets/home.jpg";
 
 const Home = ({ homeRef, scrollToSection }: { homeRef: React.RefObject<HTMLElement>, scrollToSection: (s: number) => void }) => {
     return (
         <section className="flex flex-col justify-center h-screen p-0" ref={homeRef}>
             <div className="flex mt-28">
-                <div className="flex flex-col w-2/12 gap-8 my-auto">
+                <div className="flex-col hidden w-2/12 gap-8 my-auto md:flex">
                     <a href="https://www.linkedin.com/in/prateekchaurasia/"><BsLinkedin /></a>
                     <a href="https://github.com/prateek18801/"><BsGithub /></a>
                     <a href="https://www.instagram.com/pc.incognito/"><BsInstagram /></a>
                     <a href="https://wa.me/+918726127335/"><BsWhatsapp /></a>
                 </div>
-                <div className="w-5/12 text-zinc-800">
-                    <div className="mb-4 text-xl font-medium text-neutral-500">Hello</div>
-                    <div className="mb-4 text-6xl font-semibold">I'm Prateek</div>
-                    <div className="flex items-center mb-4 text-xl">
-                        <div className="h-[1px] w-24 bg-zinc-800 mr-4"></div>
+                <div className="md:w-5/12 text-zinc-800">
+                    <div className="mb-1 text-lg font-medium md:mb-4 md:text-xl text-neutral-500">Hello</div>
+                    <div className="mb-1 text-5xl font-semibold md:mb-4 md:text-6xl">I'm Prateek</div>
+                    <div className="flex items-center mb-4 text-lg md:text-xl">
+                        <div className="h-[1px] w-16 md:w-24 bg-zinc-800 mr-4"></div>
                         Web Developer 🕷🕸
                     </div>
-                    <p className="mb-8 text-neutral-500">
+                    <p className="mb-8 text-base text-neutral-500">
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Libero, et? Ipsam quod quidem est eaque!
                     </p>
                     <button className="btn-primary" onClick={() => { scrollToSection(4) }}>
@@ -26,8 +27,8 @@ const Home = ({ homeRef, scrollToSection }: { homeRef: React.RefObject<HTMLEleme
                         <PiPaperPlaneTiltBold />
                     </button>
                 </div>
-                <div className="flex items-center justify-center w-5/12">
-                    <div className="w-4/6 bg-purple-800 aspect-square home-image"></div>
+                <div className="items-center justify-center hidden w-5/12 md:flex">
+                    <img src={HomeImg} alt="no alt" className="w-4/6 rounded-full" />
                 </div>
             </div>
             <div className="flex justify-center mt-32 text-3xl">
