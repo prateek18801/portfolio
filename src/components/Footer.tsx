0573d7a1dc937135afcb00f06060497bda1de239
Prateek Chaurasia <prateek18801@gmail.com>
2023-11-03 21:43:27 +0530

    Fix mobile view

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,12 +3,12 @@ import { BsLinkedin, BsInstagram, BsGithub, BsWhatsapp } from "react-icons/bs";
 const Footer = ({ scrollToSection }: { scrollToSection: (s: number) => void }) => {
     return (
         <footer className="flex flex-col items-center w-full py-8 mt-16 text-white bg-zinc-900">
-            <div className="flex justify-between w-full max-w-5xl">
-                <div className="flex flex-col mb-8 text-2xl font-semibold">
+            <div className="flex flex-wrap justify-center w-full max-w-5xl px-8 mb-16 md:mb-0 md:justify-between">
+                <div className="flex flex-col items-center mb-8 text-2xl font-semibold md:items-start">
                     Prateek Chaurasia
                     <div className="text-sm font-normal">Web Developer</div>
                 </div>
-                <ul className="flex gap-6">
+                <ul className="flex gap-6 text-sm">
                     <li className="cursor-pointer" onClick={() => { scrollToSection(2) }}>Services</li>
                     <li className="cursor-pointer" onClick={() => { scrollToSection(3) }}>Portfolio</li>
                     <li className="cursor-pointer" onClick={() => { scrollToSection(4) }}>Contact</li>
