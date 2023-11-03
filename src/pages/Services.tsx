8720bd474e34b4150fb5c452421c6c3dc6b52d89
Prateek Chaurasia <prateek18801@gmail.com>
2023-11-03 21:44:32 +0530

    Fix mobile view

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -12,7 +12,7 @@ const Services = ({ servicesRef }: { servicesRef: React.RefObject<HTMLElement> }
     return (
         <section ref={servicesRef}>
             <div className="section-heading">Services</div>
-            <div className="flex justify-center gap-8">
+            <div className="flex flex-wrap justify-center gap-8">
                 <ServiceCard icon={<FaCode />} l1="Web" l2="Application" />
                 <ServiceCard icon={<TbSettingsCode />} l1="Custom" l2="Software" />
             </div>
@@ -22,7 +22,7 @@ const Services = ({ servicesRef }: { servicesRef: React.RefObject<HTMLElement> }
 
 const ServiceCard = ({ icon, l1, l2 }: ServiceCardProps) => {
     return (
-        <div className="w-1/3 p-8 transition-shadow rounded shadow hover:shadow-lg">
+        <div className="w-full p-8 transition-shadow rounded shadow md:w-1/3 hover:shadow-lg">
             <div className="mt-12 mb-8 text-3xl text-neutral-500">{icon}</div>
             <div className="mb-6 text-xl font-medium">{l1} <br /> {l2}</div>
             <div className="flex items-center text-sm font-medium cursor-pointer text-neutral-500 group">
